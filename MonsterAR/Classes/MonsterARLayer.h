b0bd4e8042d847bf265d2e27759440eebbd4f50b
Jamie White <hello@jamie-white.com>
2013-10-31 14:03:17 +0000

    Initial Commit

---
+++ MonsterAR/Classes/MonsterARLayer.h
@@ -0,0 +1,17 @@
+/**
+ *  MonsterARLayer.h
+ *  MonsterAR
+ *
+ *  Created by Jamie White on 31/10/2013.
+ *  Copyright Rhythm Digital Ltd 2013. All rights reserved.
+ */
+
+
+#import "CC3Layer.h"
+
+
+/** A sample application-specific CC3Layer subclass. */
+@interface MonsterARLayer : CC3Layer {
+}
+
+@end
